f5ef8cb09a93ce40d44c9f173284ad1ff85958f5
motemen <motemen@gmail.com>
2016-03-16 13:20:08 +0900

    lgtm.sh 0.4.0

--- lgtm.sh.rb
+++ lgtm.sh.rb
@@ -1,9 +1,9 @@
 class LgtmSh < Formula
   desc "The fastest way to generate animated LGTM images"
   homepage "https://github.com/motemen/lgtm.sh"
-  url "https://github.com/motemen/lgtm.sh/archive/v0.3.0.tar.gz"
-  sha256 "58a406c3c3de89992f61198ea7a1783d1643d123fb27dc2e0db693a29728f121"
-  version "0.3.0"
+  url "https://github.com/motemen/lgtm.sh/archive/v0.4.0.tar.gz"
+  sha256 "7eabe5d944ec0f7118820575ad53b047234695fc60c4ab931a9b2ecd904f6ae0"
+  version "0.4.0"
 
   depends_on "pup"
 
